bfddc5808a18461788ea6e11f8c77041e8f91e44
Shinigaminai <24736346+Shinigaminai@users.noreply.github.com>
2024-12-09 14:47:55 +0100

    remove filetype check

--- monitor.sh
+++ monitor.sh
@@ -2,18 +2,19 @@
 
 file_modified() {
     TIMESTAMP=$(date)
-    echo "[$TIMESTAMP]: The file $1$2 was modified" >>monitor_log
+    echo "[$TIMESTAMP]: The file $1$2 was modified" >>/monitor.log
 }
 
 file_created() {
     TIMESTAMP=$(date)
-    echo "[$TIMESTAMP]: The file $1$2 was created" >>monitor_log
-    if [[ $2 == =~ (\.odt|\.doc|\.docx)$ ]]; then
-        libreoffice --headless --convert-to epub $1$2
-    fi
+    FILETYPE=$(file --mime-type -b $2)
+    echo "[$TIMESTAMP]: The file $1$2 of type $FILETYPE was created" >>/monitor.log
+    libreoffice --headless --convert-to epub $1$2 >>/monitor.log
+    mv "${FILE%.*}.epub" /usr/share/nginx/files/
 }
 
 echo "Setting up upload folder monitoring"
+echo "Monitoring started" >/monitor.log
 inotifywait -q -m -r -e modify,create /usr/share/nginx/files | while read DIRECTORY EVENT FILE; do
     case $EVENT in
     MODIFY*)
