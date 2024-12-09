9f3f56f3b8d74d25f6743a6e0f3aa1de7101b042
Shinigaminai <24736346+Shinigaminai@users.noreply.github.com>
2024-12-09 15:18:44 +0100

    moved monitor.sh into /usr/share/nginx folder

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,9 @@ FROM nginx:alpine
 
 # COPY static-html-directory /usr/share/nginx/html
 COPY nginx.conf /etc/nginx/conf.d/default.conf
-COPY monitor.sh /docker-entrypoint.d/monitor.sh
+COPY monitor.sh /usr/share/nginx/monitor.sh
+RUN chmod +x /usr/share/nginx/monitor.sh
+RUN echo "/usr/share/nginx/monitor.sh &" > /docker-entrypoint.d/monitor.sh
 RUN chmod +x /docker-entrypoint.d/monitor.sh
 RUN mkdir /usr/share/nginx/files
 RUN chmod 777 /usr/share/nginx/files
--- monitor.sh
+++ monitor.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
+LOGFILE=/usr/share/nginx/html/monitor.log
+
 file_modified() {
     TIMESTAMP=$(date)
-    echo "[$TIMESTAMP]: The file $1$2 was modified" >>/monitor.log
-}
+    echo "[$TIMESTAMP]: The file $1$2 was modified" >>$LOGFILE
 
 file_created() {
     TIMESTAMP=$(date)
     FILETYPE=$(file --mime-type -b $2)
-    echo "[$TIMESTAMP]: The file $1$2 of type $FILETYPE was created" >>/monitor.log
-    libreoffice --headless --convert-to epub $1$2 >>/monitor.log
+    echo "[$TIMESTAMP]: The file $1$2 of type $FILETYPE was created" >>$LOGFILE
+    libreoffice --headless --convert-to epub $1$2 >>$LOGFILE
     mv "${FILE%.*}.epub" /usr/share/nginx/files/
 }
 
 echo "Setting up upload folder monitoring"
-echo "Monitoring started" >/monitor.log
+echo "Monitoring started" >$LOGFILE
 inotifywait -q -m -r -e modify,create /usr/share/nginx/files | while read DIRECTORY EVENT FILE; do
     case $EVENT in
     MODIFY*)
